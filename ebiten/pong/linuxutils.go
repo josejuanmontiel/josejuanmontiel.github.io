da70ad458e7ef0e89909a1716b7dcaff4f1595de
Jose Juan Montiel <josejuan.montiel@gmail.com>
2022-03-06 23:56:44 +0100

    WIP

---
+++ ebiten/pong/linuxutils.go
@@ -0,0 +1,18 @@
+// //go:build linux
+
+package pong
+
+// Game is the structure of the game state
+type LinuxUtils struct {
+	Funcjs int
+}
+
+func (j *LinuxUtils) DisplayMessage(score int) {
+}
+
+// MyGoFunc returns a JavaScript function
+func (j *LinuxUtils) MyGoFunc() {
+}
+
+func (j *LinuxUtils) RegisterGlobalFunc() {
+}
